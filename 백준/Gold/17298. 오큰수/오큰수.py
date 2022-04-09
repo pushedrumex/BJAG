c0c2b11afb390c35e8eb21978119da80af54f5c3
pushedrumex <97938489+pushedrumex@users.noreply.github.com>
2022-04-09 19:23:40 +0900

    Update 오큰수.py

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,10 +1,13 @@
 N = int(input())
 T = list(map(int,input().split()))
+
 answer = [-1] * N
-stack = [] # 인덱스 
-stack.append(0) 
+stack = []   # 내림차순으로 값이 들어오고 이에 해당하는 인덱스가 쌓임 
+stack.append(0)
+
 for i in range(1,N):
   while stack and T[stack[-1]] < T[i]:
     answer[stack.pop()] = T[i]
   stack.append(i)
+  
 print(*answer)
