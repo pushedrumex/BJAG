cd0879bf7bf3df8fe995f4f0b3ce51fb79474357
pushedrumex <97938489+pushedrumex@users.noreply.github.com>
2022-04-10 15:30:13 +0900

    Update 카드 구매하기.py

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,6 +1,9 @@
 N = int(input())
+
 d = [0] + list(map(int,input().split()))
-for i in range(2,N+1):
+
+for i in range(2,N+1):   # 2개부터 차례대로 최대금액 찾기
     for j in range(i//2 + 1):
         if d[i] < d[j] + d[i-j]:d[i] = d[j] + d[i-j]
+            
 print(d[N])
