a626de8ba764a253a6b617b7d980cf14935593fb
pushedrumex <97938489+pushedrumex@users.noreply.github.com>
2022-04-13 01:56:12 +0900

    [Silver IV] Title: 보물, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,9 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+sum = 0
+A.sort()
+B.sort(reverse=True)
+for i in range(N):
+    sum += A[i]*B[i]
+print(sum)
