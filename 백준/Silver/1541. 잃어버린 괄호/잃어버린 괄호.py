6c81d0781c145e7a8ec5c690512052a3796d039e
pushedrumex <97938489+pushedrumex@users.noreply.github.com>
2022-04-13 02:30:39 +0900

    Update 잃어버린 괄호.py

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -2,7 +2,8 @@ s = list(input().split('-'))
 add = list(map(int, s[0].split('+')))
 sub = []
 result = 0
+
 for i in add: result += i
 for i in range(1,len(s)): sub += list(map(int,s[i].split('+')))
-for i in sub: result -= i
+for i in sub: result -= i  # '-'뒤의 숫자들은 다 빼줌
 print(result)
