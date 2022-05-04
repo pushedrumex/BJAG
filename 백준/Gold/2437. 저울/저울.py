e937b921ac985eb225d91fb4333d4728de29e8c0
pushedrumex <97938489+pushedrumex@users.noreply.github.com>
2022-05-05 04:06:03 +0900

    Update 저울.py

--- 백준/Gold/2437. 저울/저울.py
+++ 백준/Gold/2437. 저울/저울.py
@@ -5,13 +5,15 @@ W.sort() # 1 1 2 3 6 7 30
 
 target = 1
 # [0 target)
+
 for i in W:
-    # i : 추가되는 무게, [i target + i) : 추가되었을 때 가능한 범위
+    # i : 새로 추가되는 무게, [i target + i) : 추가되었을 때 가능한 범위
     if target >= i:target += i 
     # target과 i가 겹쳐진다면 [0 target + i)로 퉁칠 수 있음
     # [0 target) [i target + i) -> [0 target + i)
     else:break
-    # 퉁칠 수 없다면 빈공간 발생
+    # 겹쳐지지 않는다면 빈공간 발생
+    
 print(target)
 
 # 참고 : https://aerocode.net/392
