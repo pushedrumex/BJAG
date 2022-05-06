4c83a6944cecdf1b6d37df56e79e4d2e3156d459
pushedrumex <97938489+pushedrumex@users.noreply.github.com>
2022-05-06 17:11:52 +0900

    Update 랜선 자르기.py

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -2,13 +2,16 @@ K, N = map(int, input().split())
 L = []
 for _ in range(K):L.append(int(input()))
 
-start = 1
-end = sum(L)//N
+start = 1 # 최소
+end = sum(L)//N # 최대
 
 while start <= end:
     sum = 0
     mid = (start + end)//2
-    for l in L: sum += l//mid
-    if sum >= N: start = mid + 1; result = mid
+    
+    for l in L: sum += l//mid # 총 랜선 길이
+        
+    if sum >= N: start = mid + 1; result = mid # 필요한 랜선 개수보다 많거나 같으면 답이 될 
     elif sum < N: end = mid - 1
+        
 print(result)
