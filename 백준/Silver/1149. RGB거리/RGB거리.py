6fd57af3ef7f3c8186bcdb68808651c31267ad4f
pushedrumex <97938489+pushedrumex@users.noreply.github.com>
2022-05-16 17:19:04 +0900

    Update RGB거리.py

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,7 +1,9 @@
 N = int(input())
 RGB = []
+
 for i in range(N):
     RGB.append(list(map(int,input().split())))
     if i!=0:
         for j in range(3):RGB[i][j] += min(RGB[i-1][(j+1)%3],RGB[i-1][(j+2)%3])
+        # RGB 각각 선택했을 때, 최소값을 저장 dp
 print(min(RGB[N-1]))
