e8541c70406f4675036b0e06b2e6026b19aa3f71
pushedrumex <97938489+pushedrumex@users.noreply.github.com>
2022-06-04 17:00:38 +0900

    Update 패션왕 신해빈.py

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -1,10 +1,14 @@
 from collections import defaultdict
 T = int(input())
 for _ in range(T):
-    d = defaultdict(lambda:1)
+    d = defaultdict(int)
     n = int(input())
+    
     for _ in range(n):d[list(input().split())[1]]+=1
+        
     d = list(dict(d).values())
     result = 1
-    for i in d:result*=i
+    for i in d:result*=(i+1)
+    # 선택하지 않는 경우 +1
     print(result-1)
+    # 아무것도 선택하지 않는 경우 -1
