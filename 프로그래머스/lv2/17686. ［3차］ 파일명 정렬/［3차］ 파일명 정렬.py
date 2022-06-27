384365e111289187edfcd064b90080a4363bec00
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2022-06-27 15:29:49 +0900

    Update and rename ［3차］ 파일명 정렬.python3 to ［3차］ 파일명 정렬.py

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -4,12 +4,10 @@ def solution(files):
     for i in range(len(files)):
         for j in range(len(files[i])):
             if files[i][j].isnumeric():
-                for k in range(j,j+6):
+                for k in range(j,j+6): # 5개가 모두 숫자인 경우 고려
                     if k == len(files[i]) or not files[i][k].isnumeric():break
                 break
         d[files[i]] = [files[i][:j].lower(),int(files[i][j:k]),i]
-        
-    print(d)
     d_sorted= sorted(d.items(),key=lambda x:x[1])
     for ds in d_sorted:answer.append(ds[0])
     return answer
