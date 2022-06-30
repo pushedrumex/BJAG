210d73085f40f2cfadc929eb91f64101810f3c97
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2022-06-30 15:15:37 +0900

    Update and rename ［3차］ 방금그곡.python3 to ［3차］ 방금그곡.py

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -1,5 +1,6 @@
 from collections import deque
-def change(s):
+
+def change(s): # 올림(#)이 있다면 소문자로 변환 후 반환
     temp = deque(s)
     s = []
     while temp:
@@ -8,6 +9,7 @@ def change(s):
             s[-1] = s[-1].lower()
         else:s.append(temp.popleft())
     return "".join(s)
+
 def solution(m, mif):
     answer = ''
     d = []
@@ -16,8 +18,8 @@ def solution(m, mif):
         temp = mif[i].split(',')
         temp[-1] = change(temp[-1])
         t =  60*(int(temp[1][:2])-int(temp[0][:2])) + int(temp[1][3:5])-int(temp[0][3:5])
-        rep = temp[-1]*2
-        if t<2*len(rep):temp[-1] = rep[:t]
+        rep = temp[-1]*2 # 2번 반복했을 때의 악보
+        if t<2*len(rep):temp[-1] = rep[:t] # 완벽하게 2번 반복을 안했다면
         else:temp[-1] *= len(m)//len(temp[-1]) + 2
         if m in temp[-1]:d.append([t,len(mif)-i,temp[-2]])
     if not d:
