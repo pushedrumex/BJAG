83c7575b15c4716d1877adfb83c4854dd465401a
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2022-07-07 13:31:04 +0900

    [Gold IV] Title: 이모티콘, Time: 156 ms, Memory: 37240 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,20 +1,15 @@
-from collections import deque
+from collections import deque,defaultdict
 
 S = int(input())
-visited = [0]*1001
-visited[1] = 1
+visited = defaultdict(bool)
 q = deque([(1,0,0)]) # 현재, 보드, 시간
 
 while q:
     a = q.popleft()
     if a[0] == S:print(a[2]);break
+    if visited[(a[0],a[1])]:continue
 
-    if 1<a[0] and visited[a[0]-1]<1000:
-        q.append((a[0]-1,a[1],a[2]+1))
-        visited[a[0]-1]+=1 
-
-    if a[1] and a[0]+a[1]<=1000 and visited[a[0]+a[1]]<1000:
-        q.append((a[0]+a[1],a[1],a[2]+1))
-        visited[a[0]+a[1]]+=1
-
+    q.append((a[0]-1,a[1],a[2]+1)) 
+    if a[1]:q.append((a[0]+a[1],a[1],a[2]+1))
     q.append((a[0],a[0],a[2]+1))
+    visited[(a[0],a[1])] = True
