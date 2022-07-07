8116005340454b76368539cfc67f915082e9539d
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2022-07-07 13:37:50 +0900

    Update 이모티콘.py

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,7 +1,7 @@
 from collections import deque,defaultdict
 
 S = int(input())
-visited = defaultdict(bool)
+visited = defaultdict(bool) # key = (현재, 보드)
 q = deque([(1,0,0)]) # 현재, 보드, 시간
 
 while q:
@@ -9,7 +9,8 @@ while q:
     if a[0] == S:print(a[2]);break
     if visited[(a[0],a[1])]:continue
 
-    q.append((a[0]-1,a[1],a[2]+1)) 
-    if a[1]:q.append((a[0]+a[1],a[1],a[2]+1))
-    q.append((a[0],a[0],a[2]+1))
+    q.append((a[0]-1,a[1],a[2]+1)) # -1
+    if a[1]:q.append((a[0]+a[1],a[1],a[2]+1)) # 붙여넣기
+    q.append((a[0],a[0],a[2]+1)) # 복사
+    
     visited[(a[0],a[1])] = True
