79948e7f95aa6e24b67caf0d09791fb9578c7915
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2022-07-12 15:47:12 +0900

    Update 가장 긴 증가하는 부분 수열.py

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,10 +1,12 @@
+# 개수는 맞지만, 수열의 내용은 틀림
+
 N = int(input())
 A = list(map(int, input().split()))
 LIS = [A[0]]
 for i in A:
     if LIS[-1] == i:continue
-    elif LIS[-1] < i:LIS.append(i)
-    else:
+    elif LIS[-1] < i:LIS.append(i) # 증가하는 수라면
+    else: # 증가하는 수가 아니라면 LIS에서 i랑 같거나 가장 가까운 큰 수를 찾아서 교환
         left, right = 0, len(LIS)-1
         while left <= right:
             mid = (left + right) // 2
