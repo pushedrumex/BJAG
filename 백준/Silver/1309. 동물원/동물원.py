f70ca10e8e6a38bc1fc9109a7bbba5a5e6781167
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2022-07-15 02:30:38 +0900

    Update 동물원.py

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,8 +1,10 @@
 N = int(input())
-dp = [0]*(N+1)
 if N == 1:print(3);exit()
+dp = [0]*(N+1)
 dp[1] = 3
 dp[2] = 7
 for i in range(3,N+1):
-    dp[i] = (2*dp[i-1] + dp[i-2])%9901
-print(dp[N])
+    dp[i] = 3*dp[i-1] - (dp[i-1] - dp[i-2])
+# 00 01 10 이 추가되는 경우의 수 - (i-1)칸이 00 일때를 제외한 경우의 수
+# i와 i-1칸에 세로로 연속으로 채워지는 경우의 수를 빼준 
+print(dp[N]%9901)
