f2f0da1b45997f3b9399d741abf7c53e9a20f9d4
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-01-09 20:36:24 +0900

    Update 토마토.py

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -4,34 +4,21 @@ dydx = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 def bfs(graph):
     ripe = deque()
-    day = 0
-
+    day = 1
+    
     for y in range(N):
         for x in range(M):
             if graph[y][x] == 1:
                 ripe.append((y, x))
-    
-    ripe.append(0) # next day
 
     while ripe:
-        if (ripe[0] == 0):
-            day += 1
-            ripe.popleft()
-            
-            if (len(ripe) == 0):
-                break
-            
-            ripe.append(0) # next day
-            continue
-
         y, x = ripe.popleft()
-
         for dy, dx in dydx:
             _y, _x = y + dy, x + dx
             if _y > N - 1 or _y < 0 or _x > M - 1 or _x < 0:
                 continue
             if graph[_y][_x] == 0:
-                graph[_y][_x] = 1
+                graph[_y][_x], day = graph[y][x] + 1, graph[y][x] + 1
                 ripe.append((_y, _x))
 
     for row in graph:
@@ -43,9 +30,6 @@ def bfs(graph):
     print(day - 1)
 
 M, N = map(int, input().split())
-graph = []
-
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
+graph = [list(map(int, input().split())) for _ in range(N)]
 
 bfs(graph)
