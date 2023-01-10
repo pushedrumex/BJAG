4df42d8f6480323be017639d451b8626009e7294
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-01-10 21:36:50 +0900

    [Gold V] Title: 적록색약, Time: 64 ms, Memory: 31048 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,37 +1,34 @@
-from collections import deque 
+# dfs
+import sys
+sys.setrecursionlimit(10000)
 
 dydx = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-def bfs(is_RG , y, x, visited):
-    q = deque([(y, x)])
-    while q:
-        y, x = q.popleft()
-        for dy, dx in dydx:
-            _y, _x = y + dy, x + dx
-            if _y > N - 1 or _x > N - 1 or _y < 0 or _x < 0 or visited[_y][_x]:
-                continue
-            if graph[_y][_x] == graph[y][x]:
-                visited[_y][_x] = True
-                q.append((_y, _x))            
-            elif is_RG and graph[y][x] in "RG" and graph[_y][_x] in "RG":
-                visited[_y][_x] = True
-                q.append((_y, _x))
-    return visited
+def dfs(is_RG, y, x):
+    visited[y][x] = True
+    for dy, dx in dydx:
+        _y, _x = y + dy, x + dx
+        if _y > N - 1 or _x > N - 1 or _y < 0 or _x < 0 or visited[_y][_x]:
+            continue
+        if graph[_y][_x] == graph[y][x]:
+            dfs(is_RG, _y, _x)
+        elif is_RG and graph[y][x] in "RG" and graph[_y][_x] in "RG":
+            dfs(is_RG, _y, _x)
 
 def countArea(is_RG, N):
-    visited = [[False] * N for _ in range(N)]
     area = 0
-
     for j in range(N):
         for i in range(N):
             if not visited[j][i]:
                 area += 1
-                visited = bfs(is_RG, j, i, visited)
+                dfs(is_RG, j, i)
 
     print(area, end=" ")
 
 N = int(input())
 graph = [list(input()) for _ in range(N)]
 
+visited = [[False] * N for _ in range(N)]
 countArea(False, N)
+visited = [[False] * N for _ in range(N)]
 countArea(True, N)
