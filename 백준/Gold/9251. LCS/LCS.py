334a2d26ff4599f3b901683774f69d264fa3839f
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-01-19 15:34:57 +0900

    Update LCS.py

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -13,7 +13,7 @@ for i in range(1, len1+1):
         if str1[i-1] == str2[j-1]:
             dp[j][i] = dp[j-1][i-1] + 1
         # 같은 알파벳이 아니라면
-        # 현재까지의 값중 큰 
+        # 현재까지의 값 중 가장 큰 값
         else:
             dp[j][i] = max(dp[j-1][i], dp[j][i-1])
 print(dp[j][i])
