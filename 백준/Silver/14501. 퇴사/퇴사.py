8606dd31825ba34d979eeb1af7bab3313b51331a
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-01-20 19:38:23 +0900

    Update 퇴사.py

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -2,6 +2,7 @@ N = int(input())
 works = [list(map(int, input().split())) for _ in range(N)]
 dp = [0] * (N+2)
 
+# 일을 선택할지 말지를 판단하기 위해 끝 날짜부터 
 for i in range(N, 0, -1):
     t, p = works[i-1]
     # 가능한 일이라면
