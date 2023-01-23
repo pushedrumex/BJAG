8da8527510a98f6889b36157fc2c70dc288d5851
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-01-23 21:25:01 +0900

    Update 동전 1.py

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,14 +1,16 @@
 n,k = map(int, input().split())
 coins = [int(input()) for _ in range(n)]
-# j와 coin이 같은 경우 = 1
+# j와 coin이 같은 경우 dp[j-coin] = 1
 dp = [1]+[0]*k
 
 # 동전
 for coin in coins:
     # 1~k
     for j in range(1, k+1):
-        # 해당 동전 포함이 가능한 경우
+        # 해당 동전을 포함해서 만들 수 있는 경우
         if j - coin >= 0:
+            # 해당 동전이 없었던 경우의
+수++
             dp[j] +=  dp[j-coin]
             
 print(dp[k])
