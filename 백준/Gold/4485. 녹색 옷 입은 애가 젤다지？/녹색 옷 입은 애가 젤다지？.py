f7b6b3c52211c1bd20eb8a2bf1736783f9d8a9b0
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-01-28 18:54:10 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 124 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -0,0 +1,28 @@
+from heapq import heappop, heappush
+INF = int(1e9)
+dydx = [(1,0), (-1,0), (0,1), (0,-1)]
+
+def dijkstra():
+    distance[1][1] = graph[0][0]
+    q = []
+    heappush(q, (graph[0][0], 1, 1))
+    while q:
+        dist, y, x = heappop(q)
+        if distance[y][x] < dist: continue
+        for dy, dx in dydx:
+            _y, _x = y + dy, x + dx
+            if not (1 <= _y <= N and 1 <= _x <= N):
+                continue
+            d = dist + graph[_y-1][_x-1]
+            if distance[_y][_x] > d:
+                distance[_y][_x] = d
+                heappush(q, (d, _y, _x))
+num = 0
+while True:
+    N = int(input())
+    num += 1
+    if N == 0: break
+    graph = [list(map(int, input().split())) for _ in range(N)]
+    distance = [[INF for _ in range(N+1)] for _ in range(N+1)]
+    dijkstra()
+    print("Problem {0}: {1}".format(num, distance[N][N]))
