4353a7f03cac69c4d4e83c3e116f4f1d0f9c38ef
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-02-19 21:12:29 +0900

    [Gold IV] Title: 수 묶기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -0,0 +1,34 @@
+N = int(input())
+
+negative = []
+positive = []
+
+for _ in range(N):
+    n = int(input())
+    if n <= 0: negative.append(n)
+    else: positive.append(n)
+
+negative.sort(reverse=True)
+positive.sort()
+
+result, n1, n2 = 0, 0, 0
+while negative:
+    n1 = negative.pop()
+    if negative:
+        n2 = negative.pop()
+        result += n1 * n2
+        n1 = 0
+result += n1
+n1 = 0
+while positive:
+    n1 = positive.pop()
+    if positive:
+        n2 = positive.pop()
+        if n1 == 1 or n2 == 1:
+            result += n1 + n2
+        else:
+            result += n1 * n2
+        n1 = 0
+result += n1
+
+print(result)      
