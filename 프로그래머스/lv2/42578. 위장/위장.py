451501f43a2ae5d55167949b530ff30303bdbc34
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-02-22 20:01:07 +0900

    [level 2] Title: 위장, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+def solution(clothes):
+    dic = defaultdict(int)
+    for name, type in clothes:
+        dic[type] += 1
+    answer = 1
+    for type in dic.keys():
+        answer *= dic[type] + 1
+    return answer-1
