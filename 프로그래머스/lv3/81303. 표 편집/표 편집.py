38a00b478aac39d66f7d126c88b37140cb7372c3
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-03-16 21:16:46 +0900

    [level 3] Title: 표 편집, Time: 666.21 ms, Memory: 175 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/81303. 표 편집/표 편집.py
@@ -0,0 +1,47 @@
+# 연결 리스트
+
+def solution(n, k, cmd):
+    table = ["O"] * n
+    remove = []
+    
+    # [위, 아래]
+    graph = [[-1, 1]] # 첫번째 노드
+    for i in range(1, n-1):
+        graph.append([i-1, i+1])
+    graph.append([n-2, -1]) # 마지막 노드
+
+    for c in cmd:
+        if c == "C":
+            remove.append(k)
+            table[k] = "X"
+            node = graph[k]
+            if node[1] == -1: # 마지막 노드라면
+                k = node[0]
+                graph[node[0]][1] = -1
+            elif node[0] == -1: # 첫번째 노드라면
+                k = node[1]
+                graph[node[1]][0] = -1
+            else: # 중간 노드라면
+                graph[node[0]][1] = node[1]
+                graph[node[1]][0] = node[0]
+                k = node[1]
+        elif c == "Z":
+            idx = remove.pop()
+            node = graph[idx]
+            table[idx] = "O"
+            graph[node[0]][1] = idx
+            if node[1] != -1: graph[node[1]][0] = idx
+            if k == -1: k = idx
+        else:
+            direc, num = c.split()
+            num = int(num)
+            if direc == "U":
+                while num > 0:
+                    k = graph[k][0]
+                    num -= 1
+            else:
+                while num > 0:
+                    k = graph[k][1]
+                    num -= 1
+
+    return "".join(table)
