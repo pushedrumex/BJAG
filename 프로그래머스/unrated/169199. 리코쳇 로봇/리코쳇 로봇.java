7cdc273d3ea65f4d743468273dc2e8cc7ddeca21
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-03-21 17:08:41 +0900

    [unrated] Title: 리코쳇 로봇, Time: 4.23 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -2,45 +2,47 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] board) {
-        int[][] dxdy = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        int X = board.length;
-        int Y = board[0].length();
+        int X = board.length, Y = board[0].length();
         int x, y, cnt, _x, _y;
-        ArrayDeque<int[]> q = new ArrayDeque();
+        int[] temp;
         boolean[][] visited = new boolean[X][Y];
-
-        for (int i = 0; i < X; i++) {
-            for (int j = 0; j < Y; j++)
-                if ('R' == board[i].charAt(j)) {
+        int[][] dxdy = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        Deque<int[]> q = new ArrayDeque();
+        
+        for (int i=0; i < X; i++) {
+            for (int j=0; j < Y; j++)
+                if (board[i].charAt(j) == 'R') {
                     q.addLast(new int[]{i, j, 0});
                     break;
                 }
-            if (q.size() > 0)
+            if (!q.isEmpty())
                 break;
         }
-
-        while (q.size() > 0) {
-            int[] temp = q.removeFirst();
+        
+        while (!q.isEmpty()) {
+            temp = q.removeFirst();
             x = temp[0];
             y = temp[1];
             cnt = temp[2];
+            
             visited[x][y] = true;
-
-            for (int[] d : dxdy) {
+            
+            for (int[] d: dxdy) {
                 _x = x;
                 _y = y;
-                while (0 <= _x + d[0] && _x + d[0] < X && 0 <= _y + d[1] && _y + d[1] < Y && 'D' != board[_x + d[0]].charAt(_y + d[1])) {
+                
+                while (_x + d[0] >= 0 && _x + d[0] < X && _y + d[1] >= 0 && _y + d[1] < Y && board[_x + d[0]].charAt(_y + d[1]) != 'D') {
                     _x += d[0];
                     _y += d[1];
                 }
-
+                
                 if (visited[_x][_y])
                     continue;
-                else if ('G' == board[_x].charAt(_y))
+                else if (board[_x].charAt(_y) == 'G')
                     return cnt + 1;
-                else
-                    q.addLast(new int[]{_x, _y, cnt + 1});
-            }
+                
+                q.addLast(new int[]{_x, _y, cnt + 1});
+            } 
         }
         return -1;
     }
