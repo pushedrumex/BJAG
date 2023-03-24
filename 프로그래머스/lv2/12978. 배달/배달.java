f61cd06a352dbef571641be351ff6e40ba8e513e
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-03-24 20:17:35 +0900

    [level 2] Title: 배달, Time: 1.42 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.java
+++ 프로그래머스/lv2/12978. 배달/배달.java
@@ -4,10 +4,10 @@ class Solution {
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
         int[] distance = new int[N+1];
-        List<int[]>[] graph = new ArrayList[N+1];
-        PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
+        List<Node>[] graph = new ArrayList[N+1];
+        PriorityQueue<Node> q = new PriorityQueue<>(new Comparator<Node>() {
+            public int compare(Node o1, Node o2) {
+                return o1.distance - o2.distance;
             }
         });
         
@@ -16,31 +16,39 @@ class Solution {
         }
         
         for (int[] temp: road) {
-            graph[temp[0]].add(new int[]{temp[1], temp[2]});
-            graph[temp[1]].add(new int[]{temp[0], temp[2]});
+            int a = temp[0], b = temp[1], c = temp[2];
+            graph[a].add(new Node(b, c));
+            graph[b].add(new Node(a, c));
         }
         
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[1] = 0;
-        
-        // {거리, 노드}
-        q.add(new int[]{0, 1});
-        
+
+        q.add(new Node(1, 0));
         while (!q.isEmpty()) {
-            int[] now = q.remove();
+            Node now = q.remove();
             
-            if (distance[now[1]] < now[0]) continue;
-            if (now[0] <= K) answer++;
-                
-            for (int[] node: graph[now[1]]) {
-                int d = node[1] + now[0];
-                if (distance[node[0]] > d) {
-                    distance[node[0]] = d;
-                    q.add(new int[]{d, node[0]});
+            if (distance[now.num] < now.distance) continue;
+            if (now.distance <= K) answer++;
+
+            for (Node node: graph[now.num]) {
+                int d = node.distance + now.distance;
+                if (distance[node.num] > d) {
+                    distance[node.num] = d;
+                    q.add(new Node(node.num, d));
                 }
             }
         }
-
         return answer;
+    }
+    
+    class Node {
+        int num;
+        int distance;
+        
+        Node(int num, int distance) {
+            this.num = num;
+            this.distance = distance;
+        }
     }
 }
