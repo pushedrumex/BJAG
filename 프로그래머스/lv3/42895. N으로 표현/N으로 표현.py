e3fd4603f5454aa46b7b75efc3a78ec15f4e7673
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-03-27 18:45:10 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -1,18 +1,15 @@
-# N은 1~8번 사용 가능
-# 더하기, 빼기, 곱하기, 나누기, NNNNN ...
-
 def solution(N, number):
     dp = [[] for _ in range(9)]
     for i in range(1, 9):
         dp[i].append(int(str(N)*i))
         if dp[i][0] == number: return i
         
-        for j in range(1, i):
-            
+        for j in range(1, i // 2 + 1):
             for n1 in dp[j]:
                 for n2 in dp[i-j]:
-                    temp = [n1 + n2, n1 - n2, n1 * n2]
+                    temp = [n1 + n2, n1 - n2, n2 - n1, n1 * n2]
                     if n2 != 0: temp.append(n1 // n2)
+                    if n1 != 0: temp.append(n2 // n1)
                     for target in temp:
                         if target == number: return i
                     dp[i] += temp
--- 프로그래머스/lv3/42895. N으로 표현/README.md
+++ 프로그래머스/lv3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=python3) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 0.13 ms
+메모리: 10.4 MB, 시간: 0.02 ms
 
 ### 구분
 
