b66f521e51a187a9c66855e2e0c2bc0628d91864
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-03-27 18:54:11 +0900

    [level 3] Title: N으로 표현, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -1,17 +1,19 @@
 def solution(N, number):
-    dp = [[] for _ in range(9)]
+    dp = [set() for _ in range(9)]
     for i in range(1, 9):
-        dp[i].append(int(str(N)*i))
-        if dp[i][0] == number: return i
+        dp[i].add(int(str(N)*i))
         
         for j in range(1, i // 2 + 1):
             for n1 in dp[j]:
                 for n2 in dp[i-j]:
-                    temp = [n1 + n2, n1 - n2, n2 - n1, n1 * n2]
-                    if n2 != 0: temp.append(n1 // n2)
-                    if n1 != 0: temp.append(n2 // n1)
-                    for target in temp:
-                        if target == number: return i
-                    dp[i] += temp
+                    dp[i].add(n1 + n2)
+                    dp[i].add(n1 - n2)
+                    dp[i].add(n2 - n1)
+                    dp[i].add(n1 * n2)
+                    if n2 != 0: dp[i].add(n1 // n2)
+                    if n1 != 0: dp[i].add(n2 // n1)
+
+        for target in dp[i]:
+            if target == number: return i
 
     return -1
