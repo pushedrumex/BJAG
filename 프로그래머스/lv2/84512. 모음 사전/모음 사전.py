c912e9563ca0ade009c61db5e3b385247d81df85
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-04-01 03:56:58 +0900

    [level 2] Title: 모음 사전, Time: 2.56 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,12 @@
+from itertools import product
+
+def solution(word):
+    words = []
+    for i in range(1, 6):
+        words += set(product(["A","E", "I", "O", "U"],repeat=i))
+        
+    words = sorted(list(words))
+    for i in range(len(words)):
+        if "".join(words[i]) == word:
+            return i+1
+    return -1
