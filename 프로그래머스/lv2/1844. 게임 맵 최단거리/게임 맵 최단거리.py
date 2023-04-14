989cb094d2ad09e629e9616033364ebfe43db1ab
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-04-15 01:58:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.88 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -5,7 +5,7 @@ def solution(maps):
     N, M = len(maps), len(maps[0])
     visited = [[False] * M for _ in range(N)]
     q = deque([[0,0,1]])
-    
+    visited[0][0] = True
     while q:
         x,y,cnt = q.popleft()
         if [x,y] == [N-1,M-1]: return cnt
@@ -14,5 +14,4 @@ def solution(maps):
             if not (0 <= _x < N and 0 <= _y < M) or maps[_x][_y] == 0 or visited[_x][_y]: continue
             q.append([_x,_y,cnt+1])
             visited[_x][_y] = True
-
     return -1
