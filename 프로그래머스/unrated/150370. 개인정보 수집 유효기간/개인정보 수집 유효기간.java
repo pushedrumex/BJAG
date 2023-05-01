7e2e53490604e49cafb3da7c574eed89dd395443
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-05-01 15:18:04 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 4.79 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,46 +1,30 @@
 import java.util.*;
 
 class Solution {
+    int convert(String date) {
+        String[] temp = date.split("[.]");
+        int year = Integer.parseInt(temp[0].substring(2, 4));
+        int month = Integer.parseInt(temp[1]);
+        int day = Integer.parseInt(temp[2]);
+        
+        return year*12*30+month*30+day;
+    }
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> list = new ArrayList<>();
-        int[] answer;
-        String[] temp;
+        List<Integer> answer = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
         
         for (String term: terms) {
-            temp = term.split(" ");
-            map.put(temp[0], Integer.parseInt(temp[1]));
+            String[] temp = term.split(" ");
+            map.put(temp[0], Integer.parseInt(temp[1])*30);
         }
         
-        for (int i=0;i < privacies.length;i++) {
-            temp = privacies[i].split(" ");
-            if (today.compareTo(calculate(temp[0], map.get(temp[1]))) >= 0) {
-                list.add(i + 1);
-            }
+        int todayConverted = convert(today);
+        for (int i=0;i<privacies.length;i++) {
+            String[] temp = privacies[i].split(" ");
+            int destroy = convert(temp[0]) + map.get(temp[1]);
+            if (todayConverted >= destroy) answer.add(i+1);
         }
         
-        answer = new int[list.size()];
-        
-        for (int i=0;i < list.size(); i++)
-            answer[i] = list.get(i);
-        
-        return answer;
+        return answer.stream().mapToInt(integer -> integer).toArray();
     }
-    
-    String calculate(String date, int term) {
-        String monthString;
-        String[] temp = date.split("[.]");
-        int month = Integer.parseInt(temp[1]) + term;
-        int year = (Integer.parseInt(temp[0])) + month / 12;
-        month %= 12;
-        
-        if (month == 0) {
-            month = 12;
-             --year;
-        }
-        monthString = "0" + String.valueOf(month);
-        monthString = monthString.substring(monthString.length() - 2, monthString.length());
-        return String.join(".", new String[]{String.valueOf(year), monthString, temp[2]});
-    }
-    
 }
