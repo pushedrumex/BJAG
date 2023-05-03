e3e8c9e26a1d20b90ab12aeec3d0c5b1ee8c39ef
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-05-03 14:01:07 +0900

    [level 3] Title: N으로 표현, Time: 29.12 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
@@ -3,33 +3,37 @@ import java.util.*;
 class Solution {
     public int solution(int N, int number) {
         Set<Integer>[] dp = new Set[9];
-        String[] temp;
-        
-        for (int i=0;i<9;i++)
-            dp[i] = new HashSet();
         
         for (int i=1;i<9;i++) {
-            temp = new String[i];
-            Arrays.fill(temp, String.valueOf(N));
-            dp[i].add(Integer.parseInt(String.join("",temp)));
+            dp[i] = new HashSet<>();
             
-            for (int j=1;j<i/2+1;j++) {
+            StringBuffer temp = new StringBuffer();
+            int cnt = i;
+            while (cnt-- > 0) temp.append(String.valueOf(N));
+            
+            dp[i].add(Integer.parseInt(temp.toString()));
+            
+            for (int j=1;j<=i/2;j++) {
                 for (int n1: dp[j]) {
                     for (int n2: dp[i-j]) {
-                        dp[i].add(n1 + n2);
-                        dp[i].add(n1 - n2);
-                        dp[i].add(n2 - n1);
-                        dp[i].add(n1 * n2);
-                        if (n2 > 0) dp[i].add(n1 / n2);
-                        if (n1 > 0) dp[i].add(n2 / n1);
+                        dp[i].add(n1+n2);
+                        dp[i].add(n1*n2);
+                        dp[i].add(n1-n2);
+                        dp[i].add(n2-n1);
+                        if (n1 > 0) dp[i].add(n2/n1);
+                        if (n2 > 0) dp[i].add(n1/n2);
                     }
                 }
             }
-            for (int n: dp[i])
-                if (n == number)
-                    return i;      
+        }
+        
+        for (int i=1;i<9;i++) {
+            for (int n: dp[i]) {
+                if (n == number) return i;
+            }
         }
         
         return -1;
     }
 }
+
