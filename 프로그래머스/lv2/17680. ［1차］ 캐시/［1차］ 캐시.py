65dca75d42e793e783c07421df23fc53501ad8a3
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-06-08 10:27:28 +0900

    [level 2] Title: [1차] 캐시, Time: 0.69 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,16 +1,22 @@
 def solution(cacheSize, cities):
-    answer = 0
+    HIT = 1
+    MISS = 5
+    
     cache = []
-    if cacheSize == 0:return len(cities)*5
+    answer = 0
+    
+    if cacheSize == 0: return len(cities) * MISS
+
     for city in cities:
         city = city.lower()
         if city in cache:
             cache.remove(city)
             cache.append(city)
-            answer += 1
+            answer += HIT
         else:
             if len(cache) == cacheSize:
                 cache.pop(0)
             cache.append(city)
-            answer += 5
+            answer += MISS
+            
     return answer
