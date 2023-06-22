3ae27fc9c2fe0fc178e757a07f2d4113e66ac13a
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-06-22 09:56:32 +0900

    [level 2] Title: 전화번호 목록, Time: 136.27 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,12 @@
-from collections import defaultdict
-
 def solution(phone_book):
-    d = defaultdict(bool)
-    for n in phone_book:d[n] = True
-    for n in phone_book:
-        for i in range(1,len(n)):
-            if d[n[:i]]:return False
+    phone_book.sort()
+    N = len(phone_book)
+
+    for i in range(N-1):
+        for j in range(i+1, N):
+            l = len(phone_book[i])
+            if phone_book[j][:l] == phone_book[i]:
+                return False
+            else:
+                break
     return True
