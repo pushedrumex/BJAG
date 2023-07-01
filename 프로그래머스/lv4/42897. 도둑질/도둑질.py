c191cca22be27f2cb8a211efcff9c334f7a33570
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-07-01 20:53:07 +0900

    [level 4] Title: 도둑질, Time: 537.64 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/lv4/42897. 도둑질/도둑질.py
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.py
@@ -2,15 +2,12 @@ def solution(money):
     N = len(money)
     dp1 = [0] * N
     dp2 = [0] * N
+    dp1[0] = money[0]
     
-    dp1[0], dp1[1] = money[0], max(money[:2])
-    dp2[1], dp2[2] = money[1], max(money[1:3])
-    
-    # 첫번째 집 포함
-    for i in range(2, N-1):
-        dp1[i] = max(money[i]+dp1[i-2], dp1[i-1])
-    # 첫번째 집 불포함
-    for i in range(3, N):
-        dp2[i] = max(money[i]+dp2[i-2], dp2[i-1])
-    
+    for i in range(N-1):
+        dp1[i] = max(money[i] + dp1[i-2], dp1[i-1])
+        
+    for i in range(1, N):
+        dp2[i] = max(money[i] + dp2[i-2], dp2[i-1])
+
     return max(dp1[N-2], dp2[N-1])
