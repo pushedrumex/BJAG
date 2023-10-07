b8acaaef00051024420b79ffadc4bcb8ba5a7ea5
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-10-07 18:07:39 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 4272 ms, Memory: 149432 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -4,21 +4,15 @@ A = list(map(int, input().split()))
 LIS = [A[0]]
 
 for i in A:
-    # 나보다 더 크다면 바로 추가
-    if LIS[-1] < i: LIS.append(i)
-    # 나와 같다면 무시
-    elif LIS[-1] == i: continue
-
-    # 나보다 작다면 LIS 왼쪽부터 탐색해서 처음으로 나보다 큰 원소와 교환 
+    if LIS[-1] < i:LIS.append(i)
+    elif LIS[-1] == i:continue
     else:
-        left, right = 0, len(LIS) - 1
-        while left<=right:
-            mid = (left + right) // 2
-            # 가운데 숫자가 나보다 작으면
-            if LIS[mid] < i: left = mid + 1
-            # 가운데 숫자가 나보다 크거나 같으면
-            else: right = mid - 1;result = mid
-        LIS[result] = i
-print(len(LIS))
+        left = 0
+        right = len(LIS)
 
-# 주의 : LIS 실제 원소들과는 다르지만 개수는 동일
+        while left<right:
+            mid = (left + right) // 2
+            if LIS[mid] < i: left = mid + 1
+            else: right = mid
+        LIS[right] = i
+print(len(LIS))
