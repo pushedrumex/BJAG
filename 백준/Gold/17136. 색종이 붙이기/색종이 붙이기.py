a145cdaaf613884acb25cac249fe9a473ac0bbe0
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-10-22 17:00:50 +0900

    [Gold II] Title: 색종이 붙이기, Time: 652 ms, Memory: 113772 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.py
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.py
@@ -7,15 +7,6 @@ for size in sizes: dic[size] = 0
 answer = int(1e9)
 def dfs(count):
     global answer
-    temp = 0
-    for row in paper:
-        temp += sum(row)
-    if temp == 100:
-        temp = 0
-        count = 4
-    if temp == 0:
-        answer = min(answer, count)
-        return
 
     for i in range(10):
         for j in range(10):
@@ -29,7 +20,6 @@ def dfs(count):
                     # 덮는게 가능하다면
                     if flag and dic[size] < 5:
                         dic[size] += 1
-                        flag1 = False
                         for x in range(i, i+size):
                             for y in range(j, j+size):
                                 paper[x][y] = 0
@@ -41,6 +31,8 @@ def dfs(count):
                             for y in range(j, j+size):
                                 paper[x][y] = 1
                 return
+    answer = min(answer, count)
+
 dfs(0)
 
 print(answer) if answer != int(1e9) else print(-1)
