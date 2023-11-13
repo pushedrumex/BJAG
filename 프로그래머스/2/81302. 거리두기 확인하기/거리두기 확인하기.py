2fc4138a943f54c97a8d85259fe449a4a5187f00
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-11-13 15:46:34 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -0,0 +1,35 @@
+# P : 응시자, O : 책상, X : 파티션
+from collections import deque
+
+def solution(places):
+    answer = []
+    for place in places:
+        answer.append(is_possible(place))
+    return answer
+
+def is_possible(place):
+    for i in range(5):
+        for j in range(5):
+            if place[i][j] == "P":
+                temp = bfs(i, j, place)
+                if temp == 0: return 0
+    return 1
+
+dxdy = ((0,1),(1,0),(-1,0),(0,-1))
+def bfs(start_x, start_y, place):
+    visited = [[False] * 5 for _ in range(5)]
+    visited[start_x][start_y] = True
+    q = deque([(start_x, start_y)])
+    while q:
+        x, y = q.popleft()
+        for dx, dy in dxdy:
+            _x, _y = x + dx, y + dy
+            if not (0 <= _x < 5 and 0 <= _y < 5) or visited[_x][_y]: continue
+            if abs(start_x - _x) + abs(start_y - _y) > 2: continue
+            if (start_x, start_y) == (_x, _y): continue
+            if place[_x][_y] == "X": continue
+            if place[_x][_y] == "P": return 0
+            visited[_x][_y] = True
+            q.append([_x, _y])
+    return 1
+        
