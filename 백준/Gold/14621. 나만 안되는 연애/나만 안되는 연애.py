e39b4b3f0744064a96ed5a33c7cc701c95ee3233
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-03-04 20:19:46 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 460 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.py
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.py
@@ -1,39 +1,32 @@
-from heapq import heappush, heappop
-
 N, M = map(int, input().split())
 school = [""] + input().split()
-graph = [[] for _ in range(N+1)]
-q = []
+
+costs = []
 for _ in range(M):
     u, v, d = map(int, input().split())
-    if school[v] != school[u]:
-        heappush(q, (d, u, v))
-
-# 남여가 번갈아 가면서 이어져야함
-# 모든 대학교는 이어져야함
-# 최단 거리여야함
+    if school[u] != school[v]: costs.append((d, u, v))
+costs.sort()
 
 parent = [0] * (N+1)
 for i in range(1, N+1):
     parent[i] = i
 
-def find_parent(x):
+def find(x):
     if parent[x] != x:
-        parent[x] = find_parent(parent[x])
+        parent[x] = find(parent[x])
     return parent[x]
 
-def union_parent(a, b):
-    ap = find_parent(a)
-    bp = find_parent(b)
-    if ap > bp: parent[bp] = ap
-    else: parent[ap] = bp
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a > b: parent[b] = a
+    else: parent[a] = b
 
-visited = [0] * (N+1)
 answer = 0
-while q:
-    d, u, v = heappop(q)
-    if find_parent(u) != find_parent(v):
-        union_parent(u, v)
+visited = [0] * (N+1)
+for d, u, v in costs:
+    if find(u) != find(v):
+        union(u, v)
         visited[u] = 1
         visited[v] = 1
         answer += d
