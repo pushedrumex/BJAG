9609b68367e52f6d571504b7d187e3b31c15636b
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-03-19 19:00:42 +0900

    [Gold V] Title: 동전 2, Time: 356 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,18 +1,12 @@
 n, k = map(int, input().split())
 
-coins = set([int(input()) for _ in range(n)])
+coins = [int(input()) for _ in range(n)]
 
-dp = [int(1e9)] * (k+1)
+INF = int(1e9)
+dp = [INF] * (100_000 + 1)
 for coin in coins:
-    if coin <= k:
-        dp[coin] = 1
+    dp[coin] = 1
+    for i in range(coin+1, k+1):
+        dp[i] = min(dp[i], dp[i-coin] + 1)
 
-for i in range(1, k+1):
-    for coin in coins:
-        if coin <= i:
-            dp[i] = min(dp[i], dp[i-coin] + 1)
-
-if dp[k] >= int(1e9):
-    print(-1)
-else:
-    print(dp[k])
+print(dp[k] if dp[k] != INF else -1)
