8558bbdeca60f7d21828652e3d59356377bb6ae2
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-03-20 15:03:38 +0900

    [Gold V] Title: 퇴사 2, Time: 952 ms, Memory: 287680 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,22 +1,23 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-arr = [[0, 0]] + [list(map(int, input().split())) for _ in range(N)]
 
-answer = 0
+# T: 기간, P: 금액
+arr = [list(map(int, input().split())) for _ in range(N)]
+
 dp = [0] * (N+1)
-t,p = arr[N]
-# 마지막날 상담 가능하다면
-if t + N - 1 <= N:
+t, p = arr[-1]
+if t == 1:
     dp[N] = p
-
-for day in range(N-1, 0, -1):
-    t,p = arr[day]
-    if day+t-1 > N:
-        dp[day] = dp[day+1]
+    
+for i in range(N-1, 0, -1):
+    t, p = arr[i-1]
+    end = i+t-1
+    if end > N:
+        dp[i] = dp[i+1]
         continue
-    if day+t > N:
-        temp = 0
-    else:
-        temp = dp[day+t]
-    dp[day] = max(p + temp, dp[day+1])
+
+    dp[i] = max(dp[i+1], (0 if end >= N else dp[end+1])+p)
 
 print(dp[1])
