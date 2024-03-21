51a5a319e0999dd3fb21889836599bb3b8ab75ca
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-03-21 17:21:53 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,42 @@
+from collections import defaultdict, deque
+
+answer = []
+spots = []
+graph = defaultdict(list)
+visited = defaultdict(int)
+ticket_count = None
+def dfs(a, count):
+    global answer
+    
+    if count == ticket_count:
+        if len(answer) == 0:
+            answer = spots[:]
+        else:
+            answer = min(answer, spots[:])
+        return
+    
+    for b in graph[a]:
+        if visited[(a, b)] <= 0: continue
+        visited[(a, b)] -= 1
+        spots.append(b)
+        dfs(b, count + 1)
+        visited[(a, b)] += 1
+        spots.pop()
+        
+def solution(tickets):
+    global ticket_count
+    
+    ticket_count = len(tickets)
+    for a, b in tickets:
+        graph[a].append(b)
+        visited[(a, b)] += 1
+    
+    for a in graph.keys():
+        graph[a].sort()
+        
+    start = "ICN"
+    spots.append(start)
+    
+    dfs(start, 0)
+    
+    return answer
