639ef1b60b7b4dcfc48c2a0722771dc23c34015b
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-04-03 16:34:33 +0900

    [Silver II] Title: 연속합, Time: 136 ms, Memory: 38548 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,10 +1,5 @@
 n = int(input())
 dp = list(map(int,input().split()))
-
-# 왼쪽부터 쭉 더해가면서 확인
-# 앞 부분의 연속합을 포함하고 갈 지, 아니면 내 자신부터 다시 시작할 지 선택
-# 각 dp에는 왼쪽부터 시작하여 내 자신을 포함하여 가장 큰 연속합이 저장
-
 for i in range(1,n):
     dp[i] = max(dp[i],dp[i]+dp[i-1])
 print(max(dp))
