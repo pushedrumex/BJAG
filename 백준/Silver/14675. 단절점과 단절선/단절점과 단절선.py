10b9fa9894785d96b4a72662cd7770dd42fc418d
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-04-08 13:12:16 +0900

    [Silver I] Title: 단절점과 단절선, Time: 192 ms, Memory: 111888 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
@@ -1,17 +1,18 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-graph = [[] for _ in range(N+1)]
-edges = []
+count = [0] * (N+1)
 for _ in range(N-1):
     a, b = map(int, input().split())
-    edges.append((a, b))
-    graph[a].append(b)
-    graph[b].append(a)
+    count[a] += 1
+    count[b] += 1
 
 q = int(input())
 for _ in range(q):
     t, k = map(int, input().split())
     if t == 1:
-        if len(graph[k]) > 1:
+        if count[k] > 1:
             print("yes")
         else:
             print("no")
