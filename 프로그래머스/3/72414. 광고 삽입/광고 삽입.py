6525aa25bb781a29537b188d9c0bbc0cbd8ff23d
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-05-22 15:57:50 +0900

    [level 3] Title: 광고 삽입, Time: 81.26 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
@@ -1,40 +1,39 @@
+def convert_to_sec(time):
+    h, m, s = map(int, time.split(":"))
+    return h * 3600 + m * 60 + s
+
+def convert_to_time(sec):
+    h = sec // 3600
+    sec %= 3600
+    m = sec // 60
+    sec %= 60
+    return ("00" + str(h))[-2:] + ":" + ("00" + str(m))[-2:] + ":" + ("00" + str(sec))[-2:]
+
 def solution(play_time, adv_time, logs):
-    play_time = convert(play_time)
-    adv_time = convert(adv_time)    
-
-    arr = [0] * (play_time + 1)
-    
+    play_sec = convert_to_sec(play_time)
+    times = [0] * (play_sec + 2)
     for log in logs:
-        log = log.split("-")
-        start = convert(log[0])
-        end = convert(log[1])
-        arr[start] += 1
-        arr[end] -= 1
+        start, end = log.split("-")
+        start = convert_to_sec(start)
+        end = convert_to_sec(end)
+        times[start] += 1
+        times[end] -= 1
     
-    # 누적합
-    for i in range(1, play_time):
-        arr[i] += arr[i-1]
-    for i in range(1, play_time):
-        arr[i] += arr[i-1]
+    # 시청 시간 누적
+    for i in range(play_sec+1):
+        times[i] += times[i-1]
+    
+    # 총 시청 시간 누적
+    for i in range(play_sec+1):
+        times[i] += times[i-1]
         
-    max_time = 0
+    adv_sec = convert_to_sec(adv_time)   
+    max_value = times[adv_sec]
     answer = 0
-    for i in range(adv_time-1, play_time):
-        temp = arr[i] - arr[i - adv_time]
-        if temp > max_time:
-            max_time = temp
-            answer = i- adv_time + 1
-    
-    return disconvert(answer)
+    for start in range(1, play_sec - adv_sec + 1):
+        count = times[start+adv_sec-1] - times[start-1] 
+        if count > max_value:
+            max_value = count
+            answer = start
 
-def convert(t):
-    h, m, s = map(int, t.split(":"))
-    return h * 60 * 60 + m * 60 + s
-
-def disconvert(t):
-    h = t // (60*60)
-    t %= 60*60
-    m = t // 60
-    s = t % 60
-    return ("0" + str(h))[-2:] + ":" + ("0" + str(m))[-2:] + ":" + ("0" + str(s))[-2:]
-    
+    return convert_to_time(answer)
