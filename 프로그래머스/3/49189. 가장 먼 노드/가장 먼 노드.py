fc75e918cb71d2bc8e4f9c1f7664bf26a8b3d3d9
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-05-28 15:09:50 +0900

    [level 3] Title: 가장 먼 노드, Time: 52.95 ms, Memory: 20.6 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,31 +1,26 @@
-import heapq
+from collections import deque
 
 def solution(n, edge):
-    INF = int(1e9)
-    distance = [INF] * (n+1)
-    graph = [[] for _ in range(n+1)]
+    graph = [[] for _ in range(20001)]
     for a, b in edge:
         graph[a].append(b)
         graph[b].append(a)
-    q = []
-    heapq.heappush(q, (0, 1))
-    distance[1] = 0
-    while q:
-        cost, now = heapq.heappop(q)
-        if distance[now] < cost: continue
-        for node in graph[now]:
-            _cost = cost + 1
-            if distance[node] > _cost:
-                distance[node] = _cost
-                heapq.heappush(q, (_cost, node))
-
-    max_cost = 0
-    answer = 0
-    for cost in distance[1:]:
-        if max_cost < cost:
-            answer = 1
-            max_cost = cost
-        elif max_cost == cost:
-            answer += 1
     
+    distance = [-1] * 20001
+    distance[1] = 0
+    q = deque([(1, 0)])
+    while q:
+        now, dist = q.popleft()
+        for next in graph[now]:
+            if distance[next] == -1:
+                distance[next] = dist + 1
+                q.append((next, dist + 1))
+    distance.sort(reverse=True)
+    max_distance = distance[0]
+    answer = 0
+    for d in distance:
+        if d == max_distance:
+            answer += 1
+        else:
+            break
     return answer
