bb93797084b046b49fac1f27552a4fb8bd38ba68
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-06-11 12:19:24 +0900

    [level 3] Title: 사라지는 발판, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
@@ -1,36 +1,48 @@
-def solution(board, aloc, bloc):
-    return play(board, aloc, bloc, 0)
+def solution(_board, aloc, bloc):
+    global board, M, N;
+    board = _board
+    M = len(_board)
+    N = len(_board[0])
+    
+    return play(0, aloc, bloc)
 
-dxdy = ((0,1),(1,0),(-1,0),(0,-1))
-def play(board, me, op, count):
-    x, y = me
-    if board[x][y] == 0 or isFail(board, x, y):
+dxdy = ((1,0),(-1,0),(0,1),(0,-1))
+def play(count, player, rester):
+    global answer
+    
+    px, py = player
+    rx, ry = rester
+    
+    if board[px][py] == 0:
         return count
-    # 현재 상태에서 이기고 지는 플레이 결과를 기록
-    win, lose = [], []
-    board[x][y] = 0
+    
+    win = []
+    lose = []
+    board[px][py] = 0
+    move = False
     for dx, dy in dxdy:
-        _x, _y = x + dx, y + dy
-        if not (0 <= _x < len(board) and 0 <= _y < len(board[0])) or board[_x][_y] == 0:
-            continue
-
-        result = play(board, op, (_x, _y), count + 1)
-
-        if (count + result) % 2 != 0: # me가 이겼다면
-            win.append(result)
-        else: # me가 졌다면
+        _x = px + dx
+        _y = py + dy
+        
+        if oob(_x, _y): continue
+        if board[_x][_y] == 0: continue
+        move = True
+        result = play(count + 1, rester, (_x, _y))
+        
+        if (count + result) % 2 == 0:
             lose.append(result)
-    board[x][y] = 1
+        else:
+            win.append(result)
 
-    # 이길 수 있다면 빠르게 이겨야지
+    board[px][py] = 1
+    
+    if not move:
+        return count
+
     if win:
         return min(win)
-    # 뭘해도 진다면 버텨야지
+    
     return max(lose)
-
-def isFail(board, x, y):
-    for dx, dy in dxdy:
-        _x, _y = x + dx, y + dy
-        if 0 <= _x < len(board) and 0 <= _y < len(board[0]) and board[_x][_y] == 1:
-            return False
-    return True
+        
+def oob(x, y):
+    return not (0 <= x < M and 0 <= y < N)
