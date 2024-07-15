f605acc89eac030b9030d1b5f05f23f2d74b0a40
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-07-15 18:34:26 +0900

    [Gold V] Title: 두 용액, Time: 160 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,6 +1,3 @@
-# 투 포인터 알고리즘
-# 절대값이 가장 비슷한 두 숫자의 최소 합
-
 import sys
 input = sys.stdin.readline
 
@@ -14,15 +11,7 @@ answer = [sol[p1], sol[p2]]
 while p1 < p2:
     sol1, sol2 = sol[p1], sol[p2]
     temp = sol1 + sol2
-    
-    # 합이 음수인 경우
-    # p2 이동 : 더 작은 음수
-    # p1 이동 : 0이상의 정수
     if temp < 0: p1 += 1
-        
-    # 합이 0또는 양수인 경우
-    # p1 이동 : 더 큰 양수
-    # p2 이동 : 0이하의 정수
     else: p2 -= 1
     temp = abs(temp)
     if mix_min > temp:
